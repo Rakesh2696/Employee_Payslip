602f4c5007c36b77783ffbb011d5e05ea699818c
naresh.vanga <naresh.vanga@infyzterminals.com>
2021-12-17 12:19:34 +0530

    Sample Pdf added

--- Employee_Payslip/Controllers/HomeController.cs
+++ Employee_Payslip/Controllers/HomeController.cs
@@ -2,6 +2,10 @@
 using Employee_Payslip.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
 
 namespace Employee_Payslip.Controllers
 {
@@ -39,6 +43,43 @@ namespace Employee_Payslip.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult CreateDocument()
+        {
+            //Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page.Graphics;
+
+            //Set the standard font
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+            //Draw the text
+            graphics.DrawString("Hello World!!!", font, PdfBrushes.Black, new PointF(0, 0));
+
+            //Saving the PDF to the MemoryStream
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream);
+
+            //Set the position as '0'.
+            stream.Position = 0;
+
+            //Download the PDF document in the browser
+            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
+
+            fileStreamResult.FileDownloadName = "Sample.pdf";
+
+            return fileStreamResult;
+
+
+
         }
     }
 }
